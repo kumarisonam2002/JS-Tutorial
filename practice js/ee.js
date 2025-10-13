ee47c92ff26ab8970063935a193969acae5951df
Sonam Kumari <kumarisonam142002@gmail.com>
2025-10-13 12:25:59 +0530

    ee.js

---
+++ practice js/ee.js
@@ -0,0 +1,32 @@
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if(
+            char === "a" || 
+            char === "e" || 
+            char === "i" || 
+            char === "o" || 
+            char === "u"
+        ) {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+const countVow = (str) => {
+   let count = 0;
+   for (const char of str) {
+    if(
+        char === "a" ||
+        char === "e" ||
+        char === "i" ||
+        char === "o" ||
+        char === "u"
+    ) {
+        count++;
+    }
+   }
+   return count;
+}
